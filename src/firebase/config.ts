51e68d18405f141c3f9e9e9416bf8cbb0b4d276a
Jeevana Sakthi <jeevanasakthi722@gmail.com>
2026-03-23 05:32:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-483626217-cac74",
+  "appId": "1:363937242130:web:d3028b537c89c1025e8ccb",
+  "apiKey": "AIzaSyBNGGOkrBEaUJmTWbrNc9kWiHrLlAt7Tjw",
+  "authDomain": "studio-483626217-cac74.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "363937242130"
+};
